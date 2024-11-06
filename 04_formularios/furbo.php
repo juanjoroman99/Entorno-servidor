2ee17e3ef8adac34572052dfdb685d794d362b47
Juanjo Roman <rjuanjo563@gmail.com>
2024-11-06 11:26:31 +0100

    Formulario futbol

---
+++ 04_formularios/furbo.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );  
+    ?>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
+    <?php
+    /**
+     * Equipos de la liga
+     * 
+     * Nombre (letras con tilde, ñ, ç, espacios en blanco y punto)
+     * Inicial (3 letras)
+     * Liga (select con las opciones: Liga EA Sports, Liga Hypermotion,
+     * Liga primera RFEF)
+     * Ciudad (letras con tilde, ñ, ç y espacios en blanco)
+     * Fecha de fundacion (entre hoy y el 18 de diciembre de 1889)
+     * Numero de jugadores (entre 19 y 32)
+     * 
+     */
+    ?>
+    <div class="container">
+        <h1>Formulario furbo</h1>
+        <form class="col-4" action="" method="post">
+            <div class="mb-3">
+                
+            </div>
+        </form>
+    </div>
+</head>
+<body>
+<script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
