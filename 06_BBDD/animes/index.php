6d0813112ab07ba5b041542ccc87f7e50d363c80
Juanjo Roman <rjuanjo563@gmail.com>
2024-11-21 14:13:05 +0100

    Editar animes

--- 06_BBDD/animes/index.php
+++ 06_BBDD/animes/index.php
@@ -16,6 +16,14 @@
     <div class="container">
     <h1>Tabla de animes</h1>
     <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $id_anime = $_POST["id_anime"];
+            echo "<h1>$id_anime</h1>";
+            # borrar el anime
+            $sql = "DELETE FROM animes WHERE id_anime = $id_anime";
+            $_conexion -> query($sql);
+        }
+
         $sql = "SELECT * FROM animes";
         $resultado = $_conexion -> query($sql);
         /**
@@ -33,6 +41,9 @@
                 <th>Estudio</th>
                 <th>Año</th>
                 <th>Número de temporadas</th>
+                <th>Imagenes</th>
+                <th></th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
@@ -46,6 +57,18 @@
                     ?>
                     <td>
                         <img width="100" height="200" src="<?php echo $fila["imagen"] ?>">
+                    </td>
+                    <td>
+                        <a class="btn btn-primary"
+                            href="ver_anime.php?id_anime=<?php echo $fila["id_anime"] ?>">
+                            Editar
+                        </a>
+                    </td>
+                    <td>
+                        <form action="" method="post">
+                            <input type="hidden" name="id_anime" value="<?php echo $fila["id_anime"] ?>">
+                            <input class="btn btn-danger" type="submit" value="borrar">
+                        </form>
                     </td>
                     <?php
                     echo "</tr>";
